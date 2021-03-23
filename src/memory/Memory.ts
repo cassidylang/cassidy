cad490d289f7685043eaa7712817a2be2a558e6a
anapple96 <62083669+anapple96@users.noreply.github.com>
2021-03-23 18:17:14 +0700

    Basic memory

--- src/memory/Memory.ts
+++ src/memory/Memory.ts
@@ -1,5 +1,23 @@
 import * as fs from 'fs';
-class Memory {
+export class Memory {
     constructor() {}
-
+    public static addToMem(str: string) {
+        fs.appendFile('mem.txt', str,  (err) => {
+            if (err) throw err;
+            console.log('Variable data and flags pushed to memory');
+          });
+    }
+    public static removeMem(str: string) {
+        fs.unlink('mem.txt', (err) => {
+            if (err) throw err;
+            console.log('Memory cleared');
+          });
+    }
+    public static readMem(str: string) {
+        fs.readFile('mem.txt', 'utf8',(err, data) => {
+            if (err) throw err;
+            let mem = data;
+            return mem;
+        });
+    }
 }
---
+++ src/parser/Parser.ts
@@ -0,0 +1,7 @@
+import * as Memory from '../memory/Memory'
+class Parser {
+    constructor() {}
+    public static processVariable() {
+        
+    }
+}
