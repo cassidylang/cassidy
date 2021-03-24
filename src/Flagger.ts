cf023011efe11652ab1d17d38281e16dd5d819f7
Phu Minh (FreakC) <53084840+nguyenphuminh@users.noreply.github.com>
2021-03-24 18:32:57 +0700

    Update Flagger.ts

--- src/Flagger.ts
+++ src/Flagger.ts
@@ -21,6 +21,24 @@ class VariableFlag {
             case 'string':
                 type = Type.string;
                 break;
+            case 'num':
+                type = Type.num;
+                break;
+            case 'bool':
+                type = Type.bool;
+                break;
+            case 'null':
+                type = Type.null;
+                break;
+            case 'string_array':
+                type = Type.string_array;
+                break;
+            case 'num_array':
+                type = Type.num_array;
+                break;
+            case 'bool_array':
+                type = Type.bool_array;
+                break;
             default:
                 break;
         }
