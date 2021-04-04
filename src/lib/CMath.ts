e168e7a3fd08a1b0be47bc6ed3e58f24984769c1
anapple96 <62083669+anapple96@users.noreply.github.com>
2021-04-04 18:49:10 +0700

    Fixing quadratic

--- src/lib/CMath.ts
+++ src/lib/CMath.ts
@@ -73,5 +73,8 @@ export const CMath = {
     },
     quadratic: (a:number,b:number,c:number) => {
         let delta = Math.pow(b,2) - 4*a*c;
+        if (delta>0) return [(-b+Math.sqrt(delta)/2*a), (-b-Math.sqrt(delta)/2*a)];
+        else if (delta=0) return -(b/2*a);
+        else if (delta<0) return undefined;
     }
 }
