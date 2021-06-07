536c66e3510cb08b900bab15992964ca2b953813
anapple96 <62083669+anapple96@users.noreply.github.com>
2021-06-07 18:05:11 +0700

    Quick sort

--- src/lib/CArray.ts
+++ src/lib/CArray.ts
@@ -2,7 +2,6 @@ export const CArray = {
     sum: (arr: Array<number>) => arr.reduce((a, b) => a + b),
     max: (arr: Array<number>) => Math.max.apply(arr),
     min: (arr: Array<number>) => Math.min.apply(arr),
-    sort: (arr: Array<number>) => arr.sort((a: number, b: number) => a-b),
     binarysearch: (Arr: Array<number>,value: number) =>{
         let arr = CArray.sort(Arr),low = 0, high = arr.length - 1, mid;      
         while (low <= high) {
@@ -12,5 +11,48 @@ export const CArray = {
             else high = mid-1;          
         }
         return -1;
+    },
+    sort: (arr: Array<number>) => {
+        function quicksort(array: number[], left: number, right: number) {
+            if (left >= right) {
+              return
+            }
+            const pivot = array[Math.floor((left + right) / 2)]
+            const index = partition(array, left, right, pivot)
+            quicksort(array, left, index - 1)
+            quicksort(array, index, right)
+          }
+          
+          function swap(array: number[], left: number, right: number) {
+            const t = array[right]
+            array[right] = array[left]
+            array[left] = t
+          }
+          
+          function partition(
+            array: number[],
+            left: number,
+            right: number,
+            pivot: number
+          ) {
+            while (left <= right) {
+              while (array[left] < pivot) {
+                left++
+              }
+          
+              while (array[right] > pivot) {
+                right--
+              }
+          
+              if (left <= right) {
+                swap(array, left, right)
+                left++
+                right--
+              }
+            }
+            return left
+          }
+          quicksort(arr, 0, arr.length - 1)
+          return arr;
     }
 }
