3142e58a957003e00959eecbe52ab5e2c667ac8d
anapple96 <62083669+apple096@users.noreply.github.com>
2022-06-20 19:24:14 +0700

    Spacing out code

--- src/Parser.ts
+++ src/Parser.ts
@@ -3,11 +3,17 @@ import { Access, Body, Empty, FunctionCall, FunctionDeclaration, ObjectReference
 import {TokenType, Lexer, ValueType} from "./Lexer";
 export class Parser {
     constructor() {}
+    // Current parser recursive depth
     recursionCall: number = 0;
+    // Token list
     tokens = Lexer.tokens;
+    // Current pointer
     ptr: number = 0;
+    /**Peak next token*/
     next = () => Lexer.tokens[this.ptr+1];
+    /**Current token */
     current = () => Lexer.tokens[this.ptr] || TokenType.EMPTY;
+    /**Match singular token */
     eat = (t: TokenType) => {
         console.log(TokenType[t]);
         console.log(TokenType[this.current().type])
@@ -18,9 +24,14 @@ export class Parser {
             throw new Error(`Expected ${TokenType[t]}, got ${TokenType[this.current().type]}`);
         }
     }
+    /**Entry parse */
     parseProgram() {
         return new Program(this.parseStatements());
     }
+    /**
+     * Parse singular statement
+     * DO NOT CALL FOR TOP LEVEL
+     */
     parseStatement() {
         let node: Statement = new Empty();
         switch (this.current().type) {
@@ -38,6 +49,9 @@ export class Parser {
         }
         return node;
     }
+    /**
+     * Parse statements
+     */
     parseStatements() {
         let parsing = true, statements = [];
         while (parsing) {
@@ -52,73 +66,109 @@ export class Parser {
         return statements;
     }
     parsePossibleDeclaration(): Statement | Statement[] {
+
         let node: Statement | Statement[] = new Empty(),
             nodet: TokenType = TokenType.EMPTY;
+
         switch (this.current().type) {
+
             case TokenType.INT:
                 this.eat(TokenType.INT)
                 nodet = TokenType.INT
                 break;
+
             case TokenType.STRING:
                 this.eat(TokenType.STRING)
                 nodet = TokenType.STRING
                 break;
+
             case TokenType.BOOL:
                 this.eat(TokenType.BOOL)
                 nodet = TokenType.BOOL
                 break;
+
             case TokenType.VOID:
                 this.eat(TokenType.VOID)
                 nodet = TokenType.VOID
                 break;
+
         }
+
         if (this.current().type===TokenType.VALUE) {
+
             let identifier = this.current().value;
             this.eat(TokenType.VALUE);
+
             if (this.current().type===TokenType.L_PAREN) {
+
                 this.eat(TokenType.L_PAREN);
                 let args = this.parseArgs();
                 this.eat(TokenType.R_PAREN);
                 let body = this.parseBody();
+
                 node = new FunctionDeclaration(identifier, args, body, nodet);
+
             } else if (this.current().type===TokenType.SEMICOLON) {
+
                 this.eat(TokenType.SEMICOLON);
+
                 node = new VariableDeclaration(identifier, nodet);
+
             } else if (this.current().type === TokenType.ASSIGN) {
+
                 this.eat(TokenType.ASSIGN);
+
                 if (ValueType.includes(this.current().type)) {
+
                     if (this.current().type!==TokenType.VALUE) {
+
                         node = new VariableDeclaration(identifier, nodet, this.current().value);
-                        console.log(this.current());
+
                         if (this.current().type) this.eat(this.current().type)
+
                     } else {
+
                         node = new VariableDeclaration(identifier, nodet, this.parsePossibleReference());
-                        console.log(this.current());
+
                         if (this.current().type) this.eat(this.current().type)
+
                     }
+
                     if (this.current().type===TokenType.COMMA) {
+
                         let decs = [node];
                         this.eat(TokenType.COMMA)
                         let parsing = true;
+
                         while (parsing) {
+
                             if (this.current().type === TokenType.VALUE) {
+
                                 let vident = (this.current().value as string);
-                                console.log(this.current());
                                 this.eat(TokenType.VALUE)
+
                                 if (this.current().type === TokenType.ASSIGN) {
+
                                     this.eat(TokenType.ASSIGN);
+
                                     if (ValueType.includes(this.current().type)) {
+
                                         decs.push(new VariableDeclaration(vident, nodet, this.current().value));
                                         if (this.current().type) this.eat(this.current().type)
-                                    } else {
-                                        console.log("err1",this.current());
+
+                                    } else 
                                         break;
-                                    }
+
                                 }
+
                                 if (this.current().type === TokenType.COMMA) {
+
                                     this.eat(TokenType.COMMA)
+
                                 } else if (this.current().type === TokenType.SEMICOLON){
+
                                     parsing = false;
+
                                     break;
                                 }
                             }
@@ -127,45 +177,70 @@ export class Parser {
                     }
                     
                     this.eat(TokenType.SEMICOLON);
+
                 } else {
+
                     console.log("err1",this.current());
+
                 }
             } else if (this.current().type===TokenType.COMMA) {
+
                 let decs = [new VariableDeclaration(identifier, nodet)];
                 this.eat(TokenType.COMMA)
                 let parsing = true;
+
                 while (parsing) {
+
                     if (this.current().type === TokenType.VALUE) {
+
                         let vident = (this.current().value as string);
                         this.eat(TokenType.VALUE)
+
                         if (this.current().type === TokenType.ASSIGN) {
+
                             this.eat(TokenType.ASSIGN);
+
                             if (ValueType.includes(this.current().type)) {
+
                                 decs.push(new VariableDeclaration(vident, nodet, this.current().value));
                                 if (this.current().type) this.eat(this.current().type)
+
                             } else {
+
                                 console.log("err1",this.current());
                                 break;
+
                             }
                         }
                         if (this.current().type === TokenType.COMMA) {
+
                             this.eat(TokenType.COMMA)
+
                         } else if (this.current().type === TokenType.SEMICOLON){
+
                             parsing = false;
                             break;
+
                         }
                     }
                 }
+
                 node = decs;
                 this.eat(TokenType.SEMICOLON)
+
             }
         } else {
+
             let err = this.current();
+
             ErrorC.throwErr(ErrorCode.Reserved, {name: Lexer.getTokenRaw(err), col:err.startCol, line:err.line})
         }
+
         if (nodet === TokenType.VOID && node.constructor.name !== "FunctionDeclaration") {
+
             throw new Error("Bad void var")
         } 
+
         return node;
     }
     parsePossibleReference() {
@@ -225,82 +300,114 @@ export class Parser {
         return node;
     }
     parseArgs() {
+
         let args: VariableDeclaration[] = [], parsing = true;
-        if (this.current().type===TokenType.R_PAREN) return [];
+
+        if (this.current().type === TokenType.R_PAREN) return [];
+
         while (parsing) {
-            console.log("p3");
+
             let nodet: TokenType = TokenType.EMPTY;
+
             switch (this.current().type) {
+
                 case TokenType.INT:
                     this.eat(TokenType.INT)
                     nodet = TokenType.INT
                     break;
+
                 case TokenType.STRING:
                     this.eat(TokenType.STRING)
                     nodet = TokenType.STRING
                     break;
+
                 case TokenType.BOOL:
                     this.eat(TokenType.BOOL)
                     nodet = TokenType.BOOL
                     break;
+
             }
+
             if (this.current().type === TokenType.VALUE) {
+
                 let vident = this.current().value as string;
                 this.eat(TokenType.VALUE);
+
                 if (this.current().type === TokenType.ASSIGN) {
+
                     this.eat(TokenType.ASSIGN);
+
                     if (ValueType.includes(this.current().type)) {
+
                         args.push(new VariableDeclaration(vident, nodet, this.current().value));
                         if (this.current().type) this.eat(this.current().type)
+
                     } else {
+
                         console.log("err2",this.current());
                         break;
                     }
+
                 } else if (this.current().type===TokenType.R_PAREN || this.current().type===TokenType.COMMA) {
+
                     args.push(new VariableDeclaration(vident, nodet));
+
                 }
             } else {
+
                 console.log("err3",this.current());
                 parsing = false;
                 break;
+
             }
             if (this.current().type === TokenType.COMMA) {
+
                 this.eat(TokenType.COMMA)
+
             } else if (this.current().type === TokenType.R_PAREN){
+
                 parsing = false;
                 break;
             } else {
+
                 console.log("err3",this.current());
                 parsing = false;
                 break;
             }
         }
+
         return args;
     }
     parseBody() {
+
         let node = new Body([]);
+
         this.eat(TokenType.L_CURLY);
         node.statements = this.parseStatements();
         this.eat(TokenType.R_CURLY);
+
         return node;
     }
     parseReturn() {
+
         this.eat(TokenType.RETURN);
+
         let node = new Return(new Empty());
+
         switch (this.current().type) {
+
             case TokenType.VALUE:
                 node = new Return(this.parsePossibleReference());
                 break;
+
             case TokenType.VALUE_STRING:
             case TokenType.V_BOOL:
             case TokenType.V_INT:
                 node = new Return(this.current().value);
                 break;
         }
+
         this.eat(TokenType.SEMICOLON)
         return node;
     }
-    /**
-     * Expression parser
-     */
 }
